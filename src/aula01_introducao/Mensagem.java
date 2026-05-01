dca623afbd2503ade8e15bd67992c6513cbcc08b
GabrielTalys <seu_email_do_github@gmail.com>
2026-05-01 19:23:30 -0300

    Adicionando primeira classe Java com saída no terminal

---
+++ src/aula01_introducao/Mensagem.java
@@ -0,0 +1,10 @@
+package aula01_introducao;
+
+public class Mensagem {
+    public static void main (String [] args){
+        System.out.println("Meu nome è Gabriel");
+        System.out.println("Estou Estudando Java");
+        System.out.println("Este aqui será meu Repositório de evolução.");
+    }
+}
+// Este é o meu primeiro registro de evolução no Java
